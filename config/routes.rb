a835e458ad384b3c13b5fab87d6a57a405ed1f2a
Patryk <patryk.dabrowski@decathlon.com>
2017-09-03 19:41:33 +0200

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :users
   
-
+	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
 	root 'welcome#home'
   resources :beers
-  resources :best_beers
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
