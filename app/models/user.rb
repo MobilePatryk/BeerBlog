cf1dbeb2fe5b8a50011933e194923b9151906cb7
Patryk <patryk.dabrowski@decathlon.com>
2017-09-03 11:41:25 +0200

    add user vaidations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-	has_many :comments
+	has_many :comments, dependent: :destroy 
+	validates :username, presence: true, 
+	uniqueness: {case_sensitive:false},
+	length: {minimum: 3, maximum: 25 }
+	VALIDE_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length:{maximum: 105},
+	uniqueness: {case_sensitive:false},
+	format: {with: VALIDE_EMAIL_REGEX}
 end
