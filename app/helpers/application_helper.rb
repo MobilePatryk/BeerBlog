f33863eb7a2a322ed54d24fa138525b51ab5c708
Patryk <patryk.dabrowski@decathlon.com>
2017-09-04 18:29:57 +0200

    Adding some stylish for my app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
 	def avatar_for(user)
+		require 'open-uri'
+		require 'nokogiri'
 		#debugger
 		if user.avatar
-			debugger
+			#debugger
 			image_tag(user.avatar, alt: user.username, class: "img-circle")
 		else
-		user.avatar = "https://unsplash.it/200/300/?random"
+		page = Nokogiri::HTML(open("https://www.avatarapi.com/js.aspx?email=#{user.email}&size=128"))
+		user.avatar = page.css("img").attr("src").value
 		image_tag(user.avatar, alt: user.username, class: "img-circle")
 		user.save
 		end
