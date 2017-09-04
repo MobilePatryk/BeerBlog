db3022bc615e6ac2466b2c20e49d86af17ca7675
Patryk <patryk.dabrowski@decathlon.com>
2017-09-04 16:13:36 +0200

    Adding user show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def avatar_for(user)
+		#debugger
+		if user.avatar
+			debugger
+			image_tag(user.avatar, alt: user.username, class: "img-circle")
+		else
+		user.avatar = "https://unsplash.it/200/300/?random"
+		image_tag(user.avatar, alt: user.username, class: "img-circle")
+		user.save
+		end
+	end
 end
