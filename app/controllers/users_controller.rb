b96dd1e54dfacab403816212c2bc9b16f16c7801
Patryk <patryk.dabrowski@decathlon.com>
2017-10-01 15:51:44 +0200

    Change some frontend stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -23,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:succes] = "Your accoutn was update succesfuly"
       redirect_to beers_path
@@ -34,11 +33,13 @@ class UsersController < ApplicationController
 
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
